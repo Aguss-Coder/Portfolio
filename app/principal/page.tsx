aecaf7500ff830bc7752a4e320f9e384baf720a6
Aguss-Coder <luduenagustin615@gmail.com>
2024-07-03 12:45:23 -0300

    feat(principal): add about me info

--- app/principal/page.tsx
+++ app/principal/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const projects: Project[] = projectData;
   return (
     <AuroraBackground className='h-full z-0'>
-      <nav className='sticky top-0 h-10 flex justify-end items-center w-full z-50 bg-black bg-opacity-15'>
+      <nav className='sticky   top-0 h-10 flex justify-end items-center w-full z-50 bg-black bg-opacity-15'>
         <ul className='nav-list'>
           <li className='nav-list hover:opacity-50'>
             <Link href='/'>Home</Link>
@@ -46,7 +46,7 @@ export default function Home() {
       >
         <div className='flex flex-col w-[70%] m-auto'>
           <section
-            className='z-10'
+            className='z-10 pt-10'
             id='projects'
           >
             <h2 className='text-2xl w-fit'>Projects</h2>
@@ -56,7 +56,7 @@ export default function Home() {
                   key={projectData.name}
                   className='flex flex-col items-center my-4 mx-4 p-2 gap-4 rounded-lg border border-seasalt bg-black bg-opacity-30'
                 >
-                  <h3 className='text-xl'>{projectData.name}</h3>
+                  <h3 className='text-2xl'>{projectData.name}</h3>
                   <img
                     src={projectData.image}
                     alt={projectData.name}
@@ -68,7 +68,7 @@ export default function Home() {
                       href={projectData.githubLink}
                       target='_blank'
                       rel='noreferrer'
-                      className='hover:opacity-40'
+                      className='hover:opacity-40 text-xl'
                     >
                       Repository
                     </Link>
@@ -76,7 +76,7 @@ export default function Home() {
                       href={projectData.pageLink}
                       target='_blank'
                       rel='noreferrer'
-                      className='hover:opacity-40'
+                      className='hover:opacity-40 text-xl'
                     >
                       Page
                     </Link>
@@ -85,14 +85,58 @@ export default function Home() {
               ))}
             </div>
           </section>
-          <section className='z-10 mt-8' id='aboutMe'>
+          <section
+            className='z-10 mt-8'
+            id='aboutMe'
+          >
             <h2 className='text-2xl w-fit mb-2'>About Me</h2>
-            <p>
-              I'm a software developer with a passion for learning and creating
-              new things. I have experience with web development, mobile
-              development, and game development. I'm always looking for new
-              challenges and opportunities to grow.
+            <p className='ml-4'>
+              I am a dedicated Computer Engineering student with a passion for
+              developing cutting-edge Artificial Intelligence solutions and a
+              deep love for robotics. My academic journey has equipped me with a
+              solid foundation in computer science principles, and I am eager to
+              apply this knowledge to create innovative AI applications. <br />
+              <br />
+              With a strong interest in both software and hardware, I am
+              fascinated by the endless possibilities that AI and robotics offer
+              to transform industries and improve our daily lives. My goal is to
+              contribute to the development of intelligent systems that can
+              solve real-world problems, enhance human capabilities, and pave
+              the way for a smarter future.
             </p>
+            <h3 className='text-xl w-fit my-2'>Skills and Interests</h3>
+            <ul>
+              <li className='mb-4 ml-4'>
+                <b>Artificial Intelligence</b>: Proficient in machine learning,
+                deep learning, and natural language processing. Experienced in
+                designing and implementing AI models to solve complex problems.
+              </li>
+              <li className='mb-4 ml-4'>
+                <b>Robotics</b>: Enthusiastic about robotics, with hands-on
+                experience in developing and programming robotic systems.
+                Skilled in integrating AI with robotics to create intelligent
+                and autonomous machines.
+              </li>
+              <li className='mb-4 ml-4'>
+                <b>Software Development</b>: Strong background in software
+                engineering, with expertise in various programming languages and
+                tools. Adept at developing robust and scalable software
+                solutions.
+              </li>
+              <li className='mb-4 ml-4'>
+                <b>Innovation and Problem-Solving</b>: Driven by a desire to
+                innovate and find creative solutions to challenging problems.
+                Passionate about exploring new technologies and staying at the
+                forefront of AI and robotics advancements.
+              </li>
+            </ul>
+          </section>
+          <section
+            className='z-10 mt-8'
+            id='contact'
+          >
+            <h2 className='text-2xl w-fit mb-2'>Contact</h2>
+            <p>If you want to contact me, you can send me an email to </p>
           </section>
         </div>
       </motion.div>
